7b96ea046ed51bc0798e66e30d13d0f5ebd34b85
Maheshwaran M <63275540+mahesh-002@users.noreply.github.com>
2025-03-27 13:09:48 +0530

    Create script.js
    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".animated-section");
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+    
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
